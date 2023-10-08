19b7b6646882c0ce569d7f784a1be9c740f9e978
tavodevcode <tavodevcode@gmail.com>
2023-10-07 23:28:26 -0600

    📝 Added configuration the shadcn with tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,232 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
     },
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 }
+        },
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' }
+        },
+        'infinite-scroll-reverse': {
+          from: { transform: 'translateX(-100%)' },
+          to: { transform: 'translateX(0)' }
+        },
+        'text-slide-2': {
+          '0%, 40%': {
+            transform: 'translateY(0%)'
+          },
+          '50%, 90%': {
+            transform: 'translateY(-33.33%)'
+          },
+          '100%': {
+            transform: 'translateY(-66.66%)'
+          }
+        },
+        'text-slide-3': {
+          '0%, 26.66%': {
+            transform: 'translateY(0%)'
+          },
+          '33.33%, 60%': {
+            transform: 'translateY(-25%)'
+          },
+          '66.66%, 93.33%': {
+            transform: 'translateY(-50%)'
+          },
+          '100%': {
+            transform: 'translateY(-75%)'
+          }
+        },
+        'text-slide-4': {
+          '0%, 20%': {
+            transform: 'translateY(0%)'
+          },
+          '25%, 45%': {
+            transform: 'translateY(-20%)'
+          },
+          '50%, 70%': {
+            transform: 'translateY(-40%)'
+          },
+          '75%, 95%': {
+            transform: 'translateY(-60%)'
+          },
+          '100%': {
+            transform: 'translateY(-80%)'
+          }
+        },
+        'text-slide-5': {
+          '0%, 16%': {
+            transform: 'translateY(0%)'
+          },
+          '20%, 36%': {
+            transform: 'translateY(-16.66%)'
+          },
+          '40%, 56%': {
+            transform: 'translateY(-33.33%)'
+          },
+          '60%, 76%': {
+            transform: 'translateY(-50%)'
+          },
+          '80%, 96%': {
+            transform: 'translateY(-66.66%)'
+          },
+          '100%': {
+            transform: 'translateY(-83.33%)'
+          }
+        },
+        'text-slide-6': {
+          '0%, 13.33%': {
+            transform: 'translateY(0%)'
+          },
+          '16.66%, 30%': {
+            transform: 'translateY(-14.28%)'
+          },
+          '33.33%, 46.66%': {
+            transform: 'translateY(-28.57%)'
+          },
+          '50%, 63.33%': {
+            transform: 'translateY(-42.85%)'
+          },
+          '66.66%, 80%': {
+            transform: 'translateY(-57.14%)'
+          },
+          '83.33%, 96.66%': {
+            transform: 'translateY(-71.42%)'
+          },
+          '100%': {
+            transform: 'translateY(-85.71%)'
+          }
+        },
+        'text-slide-7': {
+          '0%, 11.43%': {
+            transform: 'translateY(0%)'
+          },
+          '14.28%, 25.71%': {
+            transform: 'translateY(-12.5%)'
+          },
+          '28.57%, 40%': {
+            transform: 'translateY(-25%)'
+          },
+          '42.85%, 54.28%': {
+            transform: 'translateY(-37.5%)'
+          },
+          '57.14%, 68.57%': {
+            transform: 'translateY(-50%)'
+          },
+          '71.42%, 82.85%': {
+            transform: 'translateY(-62.5%)'
+          },
+          '85.71%, 97.14%': {
+            transform: 'translateY(-75%)'
+          },
+          '100%': {
+            transform: 'translateY(-87.5%)'
+          }
+        },
+        'text-slide-8': {
+          '0%, 10%': {
+            transform: 'translateY(0%)'
+          },
+          '12.5%, 22.5%': {
+            transform: 'translateY(-11.11%)'
+          },
+          '25%, 35%': {
+            transform: 'translateY(-22.22%)'
+          },
+          '37.5%, 47.5%': {
+            transform: 'translateY(-33.33%)'
+          },
+          '50%, 60%': {
+            transform: 'translateY(-44.44%)'
+          },
+          '62.5%, 72.5%': {
+            transform: 'translateY(-55.55%)'
+          },
+          '75%, 85%': {
+            transform: 'translateY(-66.66%)'
+          },
+          '87.5%, 97.5%': {
+            transform: 'translateY(-77.77%)'
+          },
+          '100%': {
+            transform: 'translateY(-88.88%)'
+          }
+        },
+        gradient: {
+          to: { 'background-position': '200% center' }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        'infinite-scroll': 'infinite-scroll 35s linear infinite',
+        'infinite-scroll-reverse': 'infinite-scroll-reverse 35s linear infinite',
+        'text-slide-2': 'text-slide-2 5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+        'text-slide-3': 'text-slide-3 7.5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+        'text-slide-4': 'text-slide-4 10s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+        'text-slide-5': 'text-slide-5 12.5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+        'text-slide-6': 'text-slide-6 15s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+        'text-slide-7': 'text-slide-7 17.5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+        'text-slide-8': 'text-slide-8 20s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+        gradient: 'gradient 8s linear infinite'
+      }
+    }
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')]
 }
-export default config
