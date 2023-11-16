94e1a122a3a3feec6687cfa8ec70b7127aaab920
tavodevcode <tavodevcode@gmail.com>
2023-11-15 23:10:02 -0600

    📝 Add new SVG icons for various technologies

---
+++ components/svgs/vue.tsx
@@ -0,0 +1,13 @@
+export default function Vue(): React.ReactElement {
+  return (
+    <svg
+      className="fill-black dark:fill-white"
+      height="40"
+      viewBox="0 0 32 32"
+      width="40"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M24.306 4.019h-4.806l-3.5 5.537-3-5.537h-11l14 23.981 14-23.981zM5.481 6.019h3.363l7.156 12.387 7.15-12.387h3.363l-10.512 18.012z" />
+    </svg>
+  )
+}
---
+++ components/svgs/windows.tsx
@@ -0,0 +1,18 @@
+export default function Windows(): React.ReactElement {
+  return (
+    <svg
+      className="fill-black dark:fill-white"
+      enableBackground="new 0 0 30 30"
+      height="30px"
+      id="Layer_1"
+      version="1.1"
+      viewBox="0 0 30 30"
+      width="30px"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path d="M0.01,14L0,4.248l12-1.629V14H0.01z M14,2.328L29.996,0v14H14V2.328z M30,16l-0.004,14L14,27.75V16H30z M12,27.496   L0.01,25.852V16H12V27.496z" />
+      </g>
+    </svg>
+  )
+}
