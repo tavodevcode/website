41674d3eb5c0688564fc7135e81c3d3b2326ea3a
tavodevcode <tavodevcode@gmail.com>
2023-11-16 01:27:37 -0600

    📝 Add mode toggle component for switching between light and dark themes

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+
+import { Button } from '@/components/ui/button'
+
+export function ModeToggle(): React.ReactElement {
+  const { setTheme, theme } = useTheme()
+
+  return (
+    <Button
+      className="focus-visible:outline-1 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 hover:bg-transparent dark:text-gray-300"
+      size="icon"
+      variant="ghost"
+      onClick={() => {
+        theme === 'dark' ? setTheme('light') : setTheme('dark')
+      }}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
+  )
+}
