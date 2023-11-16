056c6c117a84204a1a9cd5744c8da4fa14456bb0
tavodevcode <tavodevcode@gmail.com>
2023-11-16 02:14:30 -0600

    📝 Fix font paths in localFonts.ts

--- lib/localFonts.ts
+++ lib/localFonts.ts
@@ -1,89 +1,88 @@
 import localFont from 'next/font/local'
-
 export const poppins = localFont({
   src: [
     {
-      path: '../fonts/Poppins-Thin.ttf',
+      path: '..\\fonts\\Poppins-Thin.ttf',
       weight: '100',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-ThinItalic.ttf',
+      path: '..\\fonts\\Poppins-ThinItalic.ttf',
       weight: '100',
       style: 'italic'
     },
     {
-      path: '../fonts/Poppins-ExtraLight.ttf',
+      path: '..\\fonts\\Poppins-ExtraLight.ttf',
       weight: '200',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-ExtraLightItalic.ttf',
+      path: '..\\fonts\\Poppins-ExtraLightItalic.ttf',
       weight: '200',
       style: 'italic'
     },
     {
-      path: '../fonts/Poppins-Light.ttf',
+      path: '..\\fonts\\Poppins-Light.ttf',
       weight: '300',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-LightItalic.ttf',
+      path: '..\\fonts\\Poppins-LightItalic.ttf',
       weight: '300',
       style: 'italic'
     },
     {
-      path: '../fonts/Poppins-Regular.ttf',
+      path: '..\\fonts\\Poppins-Regular.ttf',
       weight: '400',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-Italic.ttf',
+      path: '..\\fonts\\Poppins-Italic.ttf',
       weight: '400',
       style: 'italic'
     },
     {
-      path: '../fonts/Poppins-Medium.ttf',
+      path: '..\\fonts\\Poppins-Medium.ttf',
       weight: '500',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-SemiBold.ttf',
+      path: '..\\fonts\\Poppins-SemiBold.ttf',
       weight: '600',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-SemiBoldItalic.ttf',
+      path: '..\\fonts\\Poppins-SemiBoldItalic.ttf',
       weight: '600',
       style: 'italic'
     },
     {
-      path: '../fonts/Poppins-Bold.ttf',
+      path: '..\\fonts\\Poppins-Bold.ttf',
       weight: '700',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-BoldItalic.ttf',
+      path: '..\\fonts\\Poppins-BoldItalic.ttf',
       weight: '700',
       style: 'italic'
     },
     {
-      path: '../fonts/Poppins-ExtraBold.ttf',
+      path: '..\\fonts\\Poppins-ExtraBold.ttf',
       weight: '800',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-ExtraBoldItalic.ttf',
+      path: '..\\fonts\\Poppins-ExtraBoldItalic.ttf',
       weight: '800',
       style: 'italic'
     },
     {
-      path: '../fonts/Poppins-Black.ttf',
+      path: '..\\fonts\\Poppins-Black.ttf',
       weight: '900',
       style: 'normal'
     },
     {
-      path: '../fonts/Poppins-BlackItalic.ttf',
+      path: '..\\fonts\\Poppins-BlackItalic.ttf',
       weight: '900',
       style: 'italic'
     }
@@ -94,19 +93,19 @@ export const poppins = localFont({
 export const caveat = localFont({
   src: [
     {
-      path: '../fonts/Caveat-Regular.ttf',
+      path: '..\\fonts\\Caveat-Regular.ttf',
       weight: '400'
     },
     {
-      path: '../fonts/Caveat-Medium.ttf',
+      path: '..\\fonts\\Caveat-Medium.ttf',
       weight: '500'
     },
     {
-      path: '../fonts/Caveat-Semibold.ttf',
+      path: '..\\fonts\\Caveat-Semibold.ttf',
       weight: '600'
     },
     {
-      path: '../fonts/Caveat-Bold.ttf',
+      path: '..\\fonts\\Caveat-Bold.ttf',
       weight: '700'
     }
   ],
